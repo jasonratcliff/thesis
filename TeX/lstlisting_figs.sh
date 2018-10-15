7e67023e7fca21152c11cd30c8fc19ac4bea97cc
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2018-10-15 00:40:38 -0500

    Commented lstlisting_figs.sh script and _bookdown.yml with usage for generating appendix figure PDF.

--- TeX/lstlisting_figs.sh
+++ TeX/lstlisting_figs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Extract figures set by LaTeX environment lstlistings from .TeX file.
+# Extract figures set by LaTeX lstlistings environments from .TeX file.
+# Writes new .tex file that can be included in Bookdown build.
+# Usage from project directory main level: 
+# ./TeX/lstlisting_figs.sh TeX/Thesis_A-seq-analysis.tex
+# See _bookdown.yml for PDF compilation instrunctions.
+# Requires index.Rmd and associated LaTeX YAML formatting.
 tex_file=${1}
 tex_figs=${tex_file%.tex}-figs.tex
 awk '
