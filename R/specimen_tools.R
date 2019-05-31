04067af5980682b6e45868c9d4ebcfc167ea1e06
jasonratcliff <jason.ratcliff.515@gmail.com>
2019-05-30 23:52:23 -0500

    Added option to map collector name and collection number to label variable for ggplot build of individual specimens.

--- R/specimen_tools.R
+++ R/specimen_tools.R
@@ -32,9 +32,9 @@
 #' #      Taxon_a_posteriori     Collector Collection_Number
 #' # 462 Physaria acutifolia R. C. Rollins              7940
 #'
-find_spp <- function(taxa_frame,
-                     collector = NULL, collection_number = NULL, row_id = NULL,
-                     priors = FALSE, locale = FALSE, geom = FALSE) {
+find_spp <- function(taxa_frame, collector = NULL, collection_number = NULL, 
+                     row_id = NULL, priors = FALSE, locale = FALSE, 
+                     geom = FALSE, label = FALSE) {
 
   # Establish column index to subset specimen information from taxa data frame.
   col_core <- c("Taxon_a_posteriori", "Collector", "Collection_Number")
@@ -92,6 +92,23 @@ find_spp <- function(taxa_frame,
         taxa_frame[grep(collection_args[[1]],
                         taxa_frame[, collection_col]), col_total]
     }
+  }
+  
+  # Add label aesthetic variable for ggplot mapping of individual specimen.
+  if (label ==TRUE) {
+    return_df <- 
+      dplyr::mutate(return_df, 
+                    taxon_label = paste(return_df$Collector,
+                                        return_df$Collection_Number,
+                                        sep = "\n") %>%
+                      # Clean up collector(s') name
+                      map_if(., function(full_name) {
+                        grepl("[A-Z]\\. |with", full_name)
+                      }, function(name) {
+                        gsub("[A-Z]\\. ", "", name) %>%
+                          gsub("with", "&", .)
+                      }) %>% unlist()
+                    )
   }
   return(return_df)
 }
