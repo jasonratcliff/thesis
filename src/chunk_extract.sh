3f438423e9ecdeef65518d5b672c83680e12831a
jasonratcliff <jason.ratcliff.515@gmail.com>
2019-10-10 19:20:32 -0500

    Added optional argument parsing to specify .Rmd files to extract chunks.

--- src/chunk_extract.sh
+++ src/chunk_extract.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 # Extract R chunks from bookdown .Rmd files.
 
-rmd_files=(index.Rmd 0[1-6]*Rmd)
+# Set empty file, null previous contents, and establish temp file.
 touch log/chunks/rchunks.Rmd
-cat /dev/null > log/chunks/rchunks.Rmd
+if [[ -f log/chunks/rchunks.Rmd ]]
+then
+  cat /dev/null > log/chunks/rchunks.Rmd
+fi
 
+# Test for empty string to set all .Rmd files from bookdown build.
+if [[ -z "$1" ]]
+then
+  rmd_files=(index.Rmd 0[1-6]*Rmd)
+elif [[ ! "$1" =~ ".Rmd" ]]
+then
+ echo "Enter an expression matching .Rmd files."
+ exit
+else
+  rmd_files=(index.Rmd "$@" )  # Set array parameter
+fi
+
+# Iterate over array of .Rmd files to extract chunks to file.
 for rmd in "${rmd_files[@]}"; do
-  echo "${rmd}"
+  if [[ ! -f "$rmd" ]]
+  then
+    echo "$rmd" "... is not a file."
+    exit
+  fi
+  echo "Processing file: " "$rmd"
   awk '{
       if (/^```{r*/) {
           i = 1
