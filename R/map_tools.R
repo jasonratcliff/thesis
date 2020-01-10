f56e1ee569d22d6e955817e13b2245f1415cee70
jasonratcliff <jason.ratcliff.515@gmail.com>
2020-01-09 23:59:31 -0600

    Implemented tidy evaluation for specimen identification column.

--- R/map_tools.R
+++ R/map_tools.R
@@ -528,26 +528,31 @@ map_themes <- function(gg_map_obj, legend_title = "Reviewed Annotations") {
 #'   remotes::install_github("clauswilke/ggtext")
 #'
 #' @param specimen_tibble Tibble subset of specimens to construct ggtext labels.
+#' @param id_column Character vector matching specimen tibble ID column.
 #' @return Character vector of html markup named by specimen identification.
-#' 
-spp_labels <- function(specimen_tibble) {
-  
+#'
+spp_labels <- function(specimen_tibble, id_column) {
+
+  # Reassign user input as enquosure for tidy evaluation.
+  id_column_orig <- id_column  # retain original value
+  id_column <- enquo(id_column)
+
   # Add italics, html line break and non-breaking space characters.
   # Assigns tibble object of final identifications and markdown expression.
   spp_labels_tibble <-
     specimen_tibble %>%
-    dplyr::select(Taxon_a_posteriori) %>% dplyr::distinct() %>%
+    dplyr::select(., !!id_column) %>% dplyr::distinct() %>%
     dplyr::filter(!grepl("\\?|^Physaria$|^Ca\\.",
-                         x = Taxon_a_posteriori)) %>%
-    dplyr::mutate(., label = Taxon_a_posteriori %>%
+                         x = .[[!!id_column]])) %>%
+    dplyr::mutate(., label = .[[!!id_column]] %>%
                     gsub(" ssp\\. ", "*<br><span>&nbsp;&nbsp;&nbsp;</span>  ssp\\. *",
                          x = .) %>% paste0("*", .) %>% paste0(., "*"))
-  
+
   # Pluck character vector with html label and name by specimen identification.
   spp_labels_vector <- spp_labels_tibble %>% purrr::pluck("label")
   names(spp_labels_vector) <-
-    spp_labels_tibble %>% purrr::pluck("Taxon_a_posteriori")
-  
+    spp_labels_tibble %>% purrr::pluck(., id_column_orig)  # non-enquosure
+
   return(spp_labels_vector) # Return named character vector.
-  
+
 }
