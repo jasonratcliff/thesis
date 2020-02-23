2fc1099bbc6576f3c18383709026ae921b39b5a2
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2020-02-23 01:14:34 -0600

    Add subset_cords()

--- R/specimen_functions.R
+++ R/specimen_functions.R
@@ -55,3 +55,80 @@ range_split <- function(split_var) {
 
 }
 
+# Specimen Access Tools ----
+
+#' Subset specimen data by geographic coordinates.
+#'
+#' Given an input data.frame and at least one of latitude or longitude
+#' vectors with minimum and maximum values to subset coordinate data.
+#'
+#' @param specimen_tbl Data.frame with columns Latitude / Longitude.
+#'   Should be numeric vectors of decimal degree coordinates.
+#' @param Latitude Character vector length two min / max latitude.
+#' @param Longitude Character vector length two min / max longitude.
+#' @importFrom rlang .data
+#'
+#' @return Data.frame of specimens within coordinate bounds.
+#'
+#' @examples
+#' specimen_subset <- subset_coords(specimen_tbl = herbarium_specimens,
+#'                                  Latitude = c(41, 45),
+#'                                  Longitude = c(-109, -105))
+#' range(specimen_subset$Latitude)
+#' range(specimen_subset$Longitude)
+#'
+#' @export
+#'
+subset_coords <- function(specimen_tbl, Latitude = NULL, Longitude = NULL) {
+
+  # Test for at list one of latitude / longitude vectors.
+  if (is.null(Latitude) & is.null(Longitude)) {
+    stop("Enter a vector for subsetting coordinates.")
+  }
+
+  # Assign list of coordinates and test that values are ranges.
+  filter_coordinates <-
+    list("Longitude" = Longitude, "Latitude" = Latitude) %>%
+    lapply(X = ., function(coordinate) {
+      if (!is.null(coordinate) & length(coordinate) != 2) {
+        stop("Enter a coordinate range of two values.")
+      } else {
+        return(coordinate)
+      }
+    })
+
+  # Map coordinate tibble ranges with minimum / maximum coordinate values.
+  coordinate_tbl <-
+    filter_coordinates[which(lapply(filter_coordinates, is.null) == FALSE)] %>%
+    purrr::map2_dfr(.x = ., .y = names(.),
+      .f =  function(coordinate, parallel_name) {
+        if (identical(!unique(coordinate > 0), TRUE) ||
+            identical(!unique(coordinate < 0), TRUE)) {
+          coordinate_ranges <- range(coordinate)
+          } else {
+            warning("Coordinates not in the same hemisphere...")
+            }
+        dplyr::bind_rows(
+          dplyr::bind_cols(parallel = parallel_name, edge = "minimum",
+                           coordinate = min(coordinate_ranges)),
+          dplyr::bind_cols(parallel = parallel_name, edge = "maximum",
+                           coordinate = max(coordinate_ranges)))
+        })
+
+  # Construct dplyr filter statements from mapped coordinate tibble.
+  filter_statements <-
+    purrr::pmap_dfr(coordinate_tbl, function(parallel, edge, coordinate) {
+      operand <- ifelse(edge == "minimum", ">", "<")
+      filter_statement <- paste(parallel, operand, coordinate, collapse = " ")
+      dplyr::bind_cols(construct = filter_statement)
+    }) %>% purrr::pluck("construct") %>%
+    paste0("dplyr::filter(", .) %>% paste0(., ")")
+
+  # Construct data filter pipeline by coordinate ranges and return evaluation.
+  filter_expr <- paste("specimen_tbl %>%",
+                       paste(filter_statements[1:length(filter_statements) - 1],
+                             collapse = " %>% "), "%>%",
+                       filter_statements[length(filter_statements)])
+  eval(rlang::parse_expr(filter_expr))
+}
+
