a5653582141b8e0168ad022be9a2b0c5a5823a0c
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2020-11-01 23:07:31 -0600

    Refactor phylogenetics plotting functions
    - Tree Data
    - MrBayes
    - BEAST

---
+++ R/tree_bayes.R
@@ -0,0 +1,166 @@
+# MrBayes ----
+
+#' Build `ggtree` Plot
+#'
+#' Given output of [read_tree()], build a [ggtree::ggtree()] plot with
+#' single- and multi-taxa tip labels.
+#'
+#' @param joined_ggtree Tibble of `ggtree` nodes with labels and scaled geoms
+#'   from DNA specimens output by [join_bayes()].
+#' @param ggtree_layout `layout` argument passed to [ggtree::ggtree()].
+#' @inheritParams node_geoms
+#' @importFrom ggplot2 ggplot geom_point aes_
+#' @export
+#'
+#' @return `ggtree` object built from [read_tree()] and [join_bayes()] outputs.
+#'
+#' @examples
+#' joined_ggtree <-
+#'   list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'              full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   join_bayes(tree_data = ., id_column = "prior_id")
+#'
+#' # Build ggtre object from MrBayes data import.
+#' bayes_ggtree(
+#'   joined_ggtree = joined_ggtree,
+#'   id_column = "prior_id"
+#' )
+#'
+bayes_ggtree <- function(joined_ggtree, id_column,
+                         ggtree_layout = "slanted") {
+  nudge_xlim <- -(max(range(joined_ggtree$x)) * 0.05)
+  tip_sizes <- joined_ggtree$geom_size %>% purrr::keep(.x = ., ~ !is.na(.x))
+  bayes_plot <- ggtree::ggtree(joined_ggtree, layout = ggtree_layout) +
+    geom_point(
+      data = joined_ggtree %>%
+        dplyr::filter(!is.na(.data$label)),
+      mapping = aes_(color = as.name(id_column), shape = as.name(id_column)),
+      size = tip_sizes
+    ) +
+    ggplot2::geom_label(
+      data = joined_ggtree %>%
+        dplyr::filter(.data$prob != 1 & !is.na(.data$prob)),
+      nudge_x = nudge_xlim,
+      ggplot2::aes(
+        label = sprintf("%0.3f", as.numeric(.data$prob), digits = 3)),
+      fontface = "bold", fill = "lightgoldenrod", size = 4, alpha = 0.35
+    ) +
+    ggplot2::geom_text(
+      data = joined_ggtree %>% dplyr::filter(!is.na(.data$node_group)) %>%
+        dplyr::group_by(.data$node_group) %>% dplyr::slice(1),
+      ggplot2::aes(label = .data$node_group),
+      hjust = 0, vjust = 0.25, nudge_x = abs(nudge_xlim),
+      fontface = "bold", size = 3
+    ) +
+    ggplot2::geom_text(
+      data = joined_ggtree %>% dplyr::filter(is.na(.data$node_group)),
+      ggplot2::aes(label = .data$single_label),
+      hjust = 0, vjust = 0.25, nudge_x = abs(nudge_xlim),
+      fontface = "bold", size = 2.5, na.rm = TRUE
+    )
+  return(bayes_plot)
+}
+
+#' MrBayes `ggtree` Theme Layer
+#'
+#' Build list of `ggplot` theme options for MrBayes `ggtree` build.
+#'
+#' @inheritParams bayes_ggtree
+#' @inheritParams node_geoms
+#' @export
+#'
+#' @return List of `ggplot` theme specifications for `ggtree` build.
+#'
+#' @examples
+#' joined_ggtree <-
+#'   list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'              full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   join_bayes(tree_data = ., id_column = "prior_id",
+#'              scale_vector = c(4, 12))
+#'
+#' # Add theme layers to ggtree plot.
+#' bayes_ggtree(joined_ggtree, id_column = "prior_id") +
+#'   bayes_themes(joined_ggtree, id_column = "prior_id")
+#'
+bayes_themes <- function(joined_ggtree, id_column) {
+
+  # Assign HTML markdown label vector.
+  markdown_labels <- ThesisPackage::spl_labels(
+    specimen_tbl = joined_ggtree,
+    id_column = id_column
+  )
+
+  theme_layer <- list(
+    ggplot2::scale_color_manual(
+      values = ThesisPackage::spp_color, labels = markdown_labels
+    ),
+    ggplot2::scale_shape_manual(
+      values = ThesisPackage::spp_shape, labels = markdown_labels
+    ),
+    ggplot2::theme(
+      legend.text = ggtext::element_markdown(size = 8),
+      legend.background = ggplot2::element_blank(),
+      legend.title = ggplot2::element_text(
+        hjust = 0.5, size = 14, face = "bold")
+      ),
+    ggplot2::guides(
+      color = ggplot2::guide_legend(
+        override.aes = list(size = 3),
+        ncol = 2, byrow = TRUE, keyheight = 0.15, default.unit = "inch")
+    ),
+    ggtree::geom_treescale(offset = -1.5)
+  )
+  return(theme_layer)
+}
+
+#' Build ggtree Plot Grid
+#'
+#' Wrapper function to plot legend inset onto ggtree object.
+#'
+#' @inheritParams bayes_ggtree
+#' @inheritParams node_geoms
+#' @param scale_name Character scalar to set color and shape legend name.
+#' @param x_expand Numeric scalar passed to [ggplot2::expand_limits()].
+#' @param plot_x Numeric scalar for plot x position.
+#' @param plot_y Numeric scalar for plot y position.
+#' @param plot_width Numeric scalar for plot width.
+#' @param plot_height Numeric scale for plot height.
+#' @param ... Forwarding arguments to [cowplot::draw_plot()].
+#' @export
+#'
+#' @return [ggplot2::ggplot()] with [ggtree::ggtree()] and legend inset.
+#'
+#' @examples
+#' joined_ggtree <-
+#'   list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'              full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   join_bayes(tree_data = ., id_column = "prior_id",
+#'              scale_vector = c(4, 12))
+#'
+#' # Build ggtree plot grid.
+#' bayes_plot(joined_ggtree, x_expand = 0.025, plot_y = 0.45,
+#'            id_column = "prior_id", scale_name = "Priors")
+#'
+bayes_plot <- function(joined_ggtree, id_column, scale_name,
+                       x_expand = 0.02, plot_x = 0.1, plot_y = 0.5,
+                       plot_width = 0.25, plot_height = 0.5, ...) {
+  bayes_plot <-
+    bayes_ggtree(joined_ggtree, id_column = id_column) +
+    bayes_themes(joined_ggtree, id_column = id_column) +
+    ggplot2::expand_limits(x = x_expand) +
+    ggplot2::labs(colour = scale_name, shape = scale_name)
+
+  # Extract legend and build a plot grid with inset legend.
+  bayes_build <- bayes_plot +
+    ggtree::theme_tree(legend.position = "none")
+
+  ggtree_legend <- cowplot::get_legend(bayes_plot)
+
+  ggtree_grid <- cowplot::ggdraw(bayes_build) +
+    cowplot::draw_plot(plot = ggtree_legend, x = plot_x, y = plot_y,
+                       width = plot_width, height = plot_height, ...)
+  return(ggtree_grid)
+}
---
+++ R/tree_data.R
@@ -0,0 +1,235 @@
+# Tree Data ----
+
+#' Read in MrBayes Consensus Tree
+#'
+#' Given an input Nexus file with results from a Mr. Bayes run, split rows of
+#' nodes with identical sample genotypes and join in data from
+#' [ThesisPackage::dna_specimens].
+#'
+#' @param tree_file Nexus file of MrBayes results to read in by `treeio`.
+#' @export
+#'
+#' @return Tibble for [ggtree::ggtree()] plotting with joined DNA specimen data.
+#'
+#' @examples
+#' list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'            full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   dplyr::select(node, single_label, label)
+#'
+read_tree <- function(tree_file) {
+  ggtree_import <- treeio::read.beast(tree_file) %>% ggtree::fortify()
+  tree_data <- ggtree_import %>% dplyr::select("label", "node") %>%
+    purrr::pmap_dfr(.l = ., function(label, node) {
+      dplyr::bind_rows(
+        node = rep(node),
+        single_label = stringr::str_split(
+          string = label,
+          pattern = "(?<=[0-9])_(?=[A-Z])"
+        ) %>% unlist()
+      )
+    }) %>%
+    dplyr::right_join(x = ., y = ggtree_import, by = "node") %>%
+    dplyr::left_join(x = .,
+      y = ThesisPackage::dna_specimens,
+      by = c("single_label" = "label")
+    )
+  return(tree_data)
+}
+
+#' Label Multi-taxa Nodes
+#'
+#' For tips (i.e. terminal nodes) with multiple taxa split into
+#' `single_label` by [ThesisPackage::read_tree], create a new variable
+#' `node_group` to distinguish multi-sample genotype labels.
+#'
+#' @param tree_data Tibble output by [read_tree()] with ggtree node and
+#'   label variables.
+#' @export
+#'
+#' @return Tibble with `node`, `single_label`, `label`, and `node_group`
+#'   variables where `node_group` represents multi-sample terminal nodes.
+#'
+#' @examples
+#' list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'            full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   node_labels(tree_data = .)
+#'
+node_labels <- function(tree_data) {
+  ggtree_labels <- tree_data %>%
+    dplyr::select("node", "single_label", "label")
+  multi_taxa_nodes <- ggtree_labels %>%
+    dplyr::add_count(.data$node) %>% dplyr::filter(.data$n > 1) %>%
+    dplyr::arrange(.data$node) %>%
+    dplyr::group_by(.data$node, .data$n) %>%
+    dplyr::mutate(
+      node_group = dplyr::cur_group_id() %>%
+        paste0("Genotype ", ., " (n=", .data$n, ")")
+    ) %>% dplyr::ungroup()
+  ggtree_labels <- multi_taxa_nodes %>%
+    dplyr::select(-c("n")) %>%
+    dplyr::right_join(x = ., y = ggtree_labels,
+                      by = c("node", "single_label", "label")) %>%
+    dplyr::arrange(.data$node)
+  return(ggtree_labels)
+}
+
+#' Scale Node Geoms
+#'
+#' Group [ggtree::ggtree()] samples by `node` and `id_column` to
+#' scale geom point sizes for tree tips.
+#'
+#' @param scale_vector Numeric vector of length two passed to
+#'   [scales::rescale()] `to` argument.
+#' @inheritParams node_labels
+#' @inheritParams layer_specimens
+#' @export
+#'
+#' @return Tibble with `node`, `id_column`, and `geom_size` variables
+#'   where `geom_size` represents geom sizes scaled by `node` and `id_column`.
+#'
+#' @examples
+#' list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'            full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   node_geoms(tree_data = ., id_column = "prior_id")
+#'
+node_geoms <- function(tree_data, id_column, scale_vector = c(4, 12)) {
+  ggtree_geoms <- tree_data %>%
+    dplyr::select("node", "single_label", "label", !!id_column) %>%
+    dplyr::group_by(.data$node, !!rlang::sym(id_column)) %>%
+    dplyr::count(name = "geom_size") %>% dplyr::ungroup() %>%
+    dplyr::mutate(
+      geom_size = scales::rescale(.data$geom_size, to = scale_vector)
+    )
+  return(ggtree_geoms)
+}
+
+#' Join MrBayes Import with Labels and Geoms
+#'
+#' Combine condensed node labels for multi-sample tips from [node_labels()]
+#' with scaled geom sizes from [node_geoms()].
+#'
+#' @param ... Forwarding arguments to [node_geoms()] (e.g., `scale_vector`).
+#' @inheritParams node_geoms
+#' @export
+#'
+#' @return Tibble of multi-sample split node labels and geom sizes joined to
+#'   MrBayes import by [read_tree()] and [ThesisPackage::dna_specimens] data.
+#'
+#' @examples
+#' joined_ggtree <-
+#'   list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'              full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   read_tree(tree_file = .) %>%
+#'   join_bayes(tree_data = ., id_column = "prior_id")
+#'
+join_bayes <- function(tree_data, id_column, ...) {
+  joined_ggtree <- tree_data %>%
+    dplyr::left_join(x = .,
+      y = ThesisPackage::node_labels(tree_data = tree_data),
+      by = c("node", "single_label", "label")
+    ) %>%
+      dplyr::left_join(x = .,
+      y = ThesisPackage::node_geoms(
+        tree_data = tree_data, id_column = id_column, ...
+      ),
+      by = c("node", id_column)) %>%
+    dplyr::mutate(
+      geom_size = purrr::map2_dbl(
+        .x = .data$single_label, .y = .data$geom_size,
+        function(label, size) {
+          ifelse(!is.na(label), size, NA)
+        })
+    ) %>%
+    dplyr::select("node", "node_group", "geom_size", "single_label",
+                  dplyr::everything()) %>%
+    dplyr::group_by(.data$node) %>%
+    dplyr::arrange(dplyr::desc(.data$geom_size)) %>% dplyr::ungroup()
+  return(joined_ggtree)
+}
+
+# Kable Wrappers ----
+
+#' Multi-taxa Node Specimens
+#'
+#' Identify specimens with conserved DNA sequences yielding identical nodes
+#' (i.e., terminal tips) for phylogenetic inference.
+#'
+#' @param id_name Name to replace `id_column` variable in kable output.
+#' @inheritParams read_tree
+#' @inheritParams layer_specimens
+#' @export
+#'
+#' @return Tibble of specimens comprising multi-taxa nodes with identical
+#'   DNA sequences for MrBayes analysis.
+#'
+#' @examples
+#' list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'            full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   conserved_vouchers(tree_file = .,
+#'                      id_column = "prior_id", id_name = "Species")
+#'
+conserved_vouchers <- function(tree_file, id_column, id_name) {
+  tree_data <- read_tree(tree_file = tree_file)
+  conserved_specimens <- node_labels(tree_data) %>%
+    dplyr::filter(!is.na(.data$node_group)) %>%
+    dplyr::left_join(x = ., y = ThesisPackage::dna_specimens,
+                     by = c("single_label" = "label")) %>%
+    dplyr::select("node_group", !!id_column, "State",
+                  "Collector", "Collection_Number") %>%
+    dplyr::mutate(
+      Collector = purrr::map_chr(.x = .data$Collector, function(collector) {
+        gsub("[A-Z]\\. ?", "", collector) %>%
+          gsub("&|with", "and", x = .) %>%
+          gsub(" +", " ", x = .) %>% stringr::str_squish(string = .)
+      })
+    ) %>%
+    dplyr::rename(
+      `Node Group` = .data$node_group,
+      !!id_name := !!id_column,
+      `Collection Number` = .data$Collection_Number
+    )
+  return(conserved_specimens)
+}
+
+#' Kable build for multi-specimen nodes.
+#'
+#' For specimens with multi-taxa nodes (i.e., terminal tips), build
+#' `kable` object with `kableExtra` formatting.
+#'
+#' @param conserved_specimens Voucher tibble output by [conserved_vouchers()]
+#' @param kable_caption Character vector for caption passed to
+#'   [knitr::kable()] argument `caption`.
+#' @param knitr_chunk Character vector passed to [knitr::kable()] argument
+#'  `format` - one of "latex" or "html".  Defined by
+#'  `opts_knit$get("rmarkdown.pandoc.to")` in `index.Rmd` of Thesis Bookdown.
+#' @export
+#'
+#' @return `kable` object with `kableExtra` formatting determined by
+#'   `knitr::opts_knit$get("rmarkdown.pandoc.to")`
+#'
+#' @examples
+#' list.files(system.file("extdata/MrBayes", package = "ThesisPackage"),
+#'            full.names = TRUE, pattern = "rITS-infile.nex.con.tre") %>%
+#'   kable_vouchers(tree_file = .,
+#'                  id_column = "prior_id", id_name = "Species") %>%
+#'   bayes_kable(kable_specimens = .,
+#'               kable_caption = "ggtree Specimens", knitr_chunk = "html")
+#'
+bayes_kable <- function(conserved_specimens, kable_caption, knitr_chunk) {
+  kable_build <-
+    knitr::kable(x = conserved_specimens,
+      caption = kable_caption, format = knitr_chunk, escape = FALSE,
+      row.names = FALSE, align=c("c", "c", "l", "l", "l")) %>%
+    kableExtra::kable_styling(
+      full_width = FALSE, font_size = 10,  latex_options= "hold_position"
+    ) %>%
+    kableExtra::row_spec(row = 0, bold = TRUE, font_size = 10) %>%
+    kableExtra::column_spec(1, border_left = TRUE) %>%
+    kableExtra::column_spec(2, width = "3.7cm") %>%
+    kableExtra::column_spec(5, border_right = TRUE, width = "2cm") %>%
+    kableExtra::collapse_rows(columns = 1:3)
+  return(kable_build)
+}
