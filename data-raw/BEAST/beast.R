8b1d6b55f979765c809ef42fa630ff4a92b4ceb0
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2020-11-19 22:47:34 -0600

    Remove outgroup from BEAST multi-locus

--- data-raw/BEAST/beast.R
+++ data-raw/BEAST/beast.R
@@ -4,22 +4,26 @@ fs::dir_create("data-raw/BEAST/NEXUS/")
 # Walk concatenated aligned subsets to write nexus files
 list.files("data-raw/sequencing/3.alignments-subset",
            full.names = TRUE) %>%
-purrr::walk(.x = ., function(fasta_file) {
+  purrr::walk(.x = ., function(fasta_file) {
 
-  # Extract locus substring from filepath.
-  locus <- fs::path_file(path = fasta_file) %>%
-    stringr::str_remove(pattern = "\\-ml\\.fasta")
+    # Extract locus substring from filepath.
+    locus <- fs::path_file(path = fasta_file) %>%
+      stringr::str_remove(pattern = "\\-ml\\.fasta")
 
-  # Read in FASTA file and remove locus from header sample IDs.
-  locus_fasta <- Biostrings::readDNAStringSet(filepath = fasta_file)
-  names(locus_fasta) <- gsub(" .+$", "", names(locus_fasta))
+    # Read in FASTA file and remove locus from header sample IDs.
+    locus_fasta <- Biostrings::readDNAStringSet(filepath = fasta_file)
+    names(locus_fasta) <- gsub(" .+$", "", names(locus_fasta))
 
-  # Calculate sequence width and write nexus file
-  char_wrap <- lapply(locus_fasta, length) %>% unique() %>% unlist()
-  ape::write.nexus.data(x = locus_fasta, interleaved = FALSE,
-                        charsperline = char_wrap,
-                        file = paste0("data-raw/BEAST/NEXUS/",
-                                      locus, ".nex"))
+    # Exclude outgroup taxa.
+    locus_fasta <-
+      locus_fasta[!grepl("^L(FEND|ARGY)", x = names(locus_fasta))]
+
+    # Calculate sequence width and write nexus file
+    char_wrap <- lapply(locus_fasta, length) %>% unique() %>% unlist()
+    ape::write.nexus.data(x = locus_fasta, interleaved = FALSE,
+                          charsperline = char_wrap,
+                          file = paste0("data-raw/BEAST/NEXUS/",
+                                        locus, ".nex"))
 })
 
 # Write subset of specimen IDs from multi-locus sample set.
