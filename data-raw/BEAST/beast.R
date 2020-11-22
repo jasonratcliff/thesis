0dbfa8d813e638f10d1d1306a4cf4e54e8de301e
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2020-11-21 22:54:07 -0600

    Update BEAST species tree taxa assignment script.

--- data-raw/BEAST/beast.R
+++ data-raw/BEAST/beast.R
@@ -1,4 +1,5 @@
 library(magrittr)
+
 fs::dir_create("data-raw/BEAST/NEXUS/")
 
 # Walk concatenated aligned subsets to write nexus files
@@ -26,10 +27,60 @@ list.files("data-raw/sequencing/3.alignments-subset",
                                         locus, ".nex"))
 })
 
-# Write subset of specimen IDs from multi-locus sample set.
-ThesisPackage::dna_specimens %>%
-  dplyr::select("label", "ID_final", # "Latitude.x", "Longitude.x",
-                "multiLocus") %>% dplyr::filter(multiLocus == TRUE) %>%
-  dplyr::select(-c("multiLocus")) %>%
-  dplyr::rename("traits" = "label", "species" = "ID_final") %>%
-readr::write_delim(x = ., path = "data-raw/BEAST/final_ids.txt", delim = "\t")
+# *BEAST ----
+
+# Exclude outgroup taxa from FASTA alignment and write NEXUS output.
+list.files("data-raw/sequencing/3.alignments-single",
+           full.names = TRUE) %>%
+
+  purrr::walk(.x = ., function(fasta_file) {
+    locus_fasta <- Biostrings::readDNAStringSet(filepath = fasta_file)
+    names(locus_fasta) <- gsub(" .+$", "", names(locus_fasta))
+    locus_fasta <-
+      locus_fasta[!grepl("^(LFEND|LARGY|POBCO)", x = names(locus_fasta))]
+
+    tree_file <-
+      fs::path(
+        "data-raw/BEAST/NEXUS/",
+        paste0(
+          gsub("-single", "", fs::path_ext_remove(basename(fasta_file))),
+          "-species"
+        ),
+        ext = "nex"
+      )
+
+    char_wrap <- lapply(locus_fasta, length) %>% unique() %>% unlist()
+    ape::write.nexus.data(
+      x = locus_fasta,
+      file = tree_file,
+      charsperline = char_wrap,
+      interleaved = FALSE
+    )
+  })
+
+# Write subset of specimen IDs for species tree taxa labels.
+spp_tree_taxa <- ThesisPackage::dna_specimens %>%
+  dplyr::select(1:11, dplyr::matches("header_")) %>%
+  dplyr::filter(
+    !grepl("^L(FEND|ARGY|POBCO)", x = .data$label)
+  ) %>%
+  # Replace unsampled / frameshift taxa headers as missing.
+  dplyr::mutate_at(
+    .vars = dplyr::vars(dplyr::matches("header_")),
+    .funs = ~ gsub("MISSING|FRAMESHIFT", NA, x = .x)
+  ) %>%
+  dplyr::select(
+    "label", "ID_final", # "Latitude.x", "Longitude.x", "multiLocus"
+   ) %>%
+  dplyr::mutate(
+    ID_final = gsub(
+      x = .data$ID_final,
+      pattern = " ",
+      replacement = "_"
+      ) %>%
+      gsub(pattern = "\\.", replacement = "", x = .)
+  ) %>%
+  dplyr::rename(
+    "traits" = "label",
+    "species" = "ID_final"
+  )
