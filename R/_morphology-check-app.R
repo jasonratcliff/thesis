4201f23f28378a4ffc144d19bc6198628fcc8fa4
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-02-14 22:18:58 -0600

    Simple shiny app to subset trait data

---
+++ R/_morphology-check-app.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(knitr)
+
+library(ThesisPackage)
+library(magrittr)
+library(dplyr)
+
+ui <- fluidPage(
+  titlePanel("Morphology Check"),
+  mainPanel(
+    fluidRow(
+      column(3,
+        textInput("collector", label = "Collector", value = NA),
+        textInput("collection", label = "Collection", value = NA),
+        actionButton("search", "Search!")
+      ),
+    ),
+    "<br>",
+    tabPanel("Specimens", tableOutput("morphologyTable"))
+  )
+)
+
+server <- function(input, output, session) {
+  search <- reactive({
+    req(input$search)
+    ThesisPackage::herbarium_specimens %>%
+      dplyr::filter(.data = .,
+        grepl(pattern = input$collector, x = .data$Collector),
+        grepl(pattern = input$collection, x = .data$Collection_Number)
+      )
+  })
+  
+  output$morphologyTable <- function() {
+     search() %>%
+      dplyr::select(.data = ., Collector, Collection_Number,
+                    prior_id, Taxon_a_posteriori,
+                    Fruit_trichomes, Ovule_number, Replum_shape,
+                    Basal_leaf_margins, Mature_fruit_apices) %>%
+      kable(x = .) %>%
+      kableExtra::kable_styling(kable_input = ., 
+                                bootstrap_options = "bordered")
+  }
+}
+
+# Run app ----
+shinyApp(ui, server)
