19d4ea59ad22b9f0f928772bd7e036e4aaad26d5
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-03-12 21:05:34 -0600

    Add polygon to P. vitulifera / SEINet specimens

--- R/FigResultsElevationSEINet.R
+++ R/FigResultsElevationSEINet.R
@@ -1,4 +1,5 @@
 library(ThesisPackage)
+library(dplyr)
 library(ggplot2)
 library(cowplot)
 
@@ -11,28 +12,43 @@ spp_colorado <-
     Latitude = c(36.79328, 42.21372),
     Longitude = c(-110.3819, -103.3507)
   ) %>%
-  dplyr::group_by(Taxon_a_posteriori) %>% dplyr::add_tally() %>%
-  dplyr::filter(n > 3, !is.na(Taxon_a_posteriori),
-                !grepl(pattern = "\\?", x = Taxon_a_posteriori)) %>%
-  dplyr::ungroup() %>%
-  dplyr::select("Taxon_a_posteriori", "Longitude", "Latitude") %>%
-  dplyr::rename("scientificName" = "Taxon_a_posteriori")
+  group_by(Taxon_a_posteriori) %>%
+  add_tally() %>%
+  filter(
+    n > 3,
+    !is.na(Taxon_a_posteriori),
+    !grepl(pattern = "\\?", x = Taxon_a_posteriori)
+  ) %>%
+  ungroup() %>%
+  select("Taxon_a_posteriori", "Longitude", "Latitude") %>%
+  rename("scientificName" = "Taxon_a_posteriori")
 
 # Filter SEINet data, bind herbarium vouchers, and sort by occurrence tally.
 spp_seinet <- seinet_coords %>%
-  dplyr::select("scientificName", "Longitude", "Latitude") %>%
-  dplyr::bind_rows(., spp_colorado) %>%
-  dplyr::filter(!is.na(Latitude), !is.na(Longitude)) %>%
-  dplyr::group_by(scientificName) %>%
-  dplyr::add_tally() %>%
-  dplyr::arrange(dplyr::desc(n)) %>%
-  dplyr::ungroup()
+  select("scientificName", "Longitude", "Latitude") %>%
+  bind_rows(., spp_colorado) %>%
+  filter(!is.na(Latitude), !is.na(Longitude)) %>%
+  group_by(scientificName) %>%
+  add_tally() %>%
+  arrange(desc(n)) %>%
+  ungroup()
 
 specimen_labels <-
-  spl_labels(specimen_tbl = spp_seinet, id_column = "scientificName")
+  spl_labels(
+    specimen_tbl = spp_seinet,
+    id_column = "scientificName"
+  )
 
 ggplot_specimens <- function() {
   list(
+    stat_ellipse(
+      data = spp_colorado %>%
+        select(Longitude, Latitude, scientificName) %>%
+        filter(grepl("vitulifera", scientificName)),
+      mapping = aes(x = Longitude, y = Latitude),
+      geom = "polygon", type = 'norm', level = 0.999,
+      fill = "black", alpha = 0.25
+    ),
     geom_jitter(
       data = spp_seinet, size = 3,
       aes(x = Longitude, y = Latitude,
@@ -67,7 +83,10 @@ ggplot_extent <-
   )
 
 ggplot_elevation <-
-  layer_elevation(specimen_tbl = ggplot_extent, raster_zoom = 7) +
+  layer_elevation(
+    specimen_tbl = ggplot_extent,
+    raster_zoom = 7
+  ) +
   layer_borders(spl_extent = ggplot_extent) +
   coord_sf(
     xlim = range(ggplot_extent[["Longitude"]]),
@@ -97,7 +116,7 @@ legend_grid <-
 
 # cowplot Grid ----
 
-FigResultsSEINet <-
+FigResultsElevationSEINet <-
   plot_grid(
     ggplot_elevation +
       theme(legend.position = "none") +
@@ -107,6 +126,6 @@ FigResultsSEINet <-
   )
 
 ThesisPackage::save_plot(
-  gg_plot = FigResultsSEINet,
+  gg_plot = FigResultsElevationSEINet,
   height = 6, width = 6
 )
