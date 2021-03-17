f508e32a49b327a946639147785f96d19c73be4a
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-03-16 20:12:09 -0500

    Add ellipses to CO endemic species

--- R/FigResultsElevationSEINet.R
+++ R/FigResultsElevationSEINet.R
@@ -44,8 +44,8 @@ ggplot_specimens <- function() {
     stat_ellipse(
       data = spp_colorado %>%
         select(Longitude, Latitude, scientificName) %>%
-        filter(grepl("vitulifera", scientificName)),
-      mapping = aes(x = Longitude, y = Latitude),
+        filter(grepl("vitulifera|bellii|medicinae", scientificName)),
+      mapping = aes(x = Longitude, y = Latitude, group = scientificName),
       geom = "polygon", type = 'norm', level = 0.999,
       fill = "black", alpha = 0.25
     ),
