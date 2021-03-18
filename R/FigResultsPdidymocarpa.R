c2fba42815824cdf19f73339a265898f696d52b1
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-03-17 23:54:42 -0500

    Separate P. didymocarpa map script
    - Update and standardize specimen filtering

--- R/FigResultsPdidymocarpa.R
+++ R/FigResultsPdidymocarpa.R
@@ -7,27 +7,35 @@ set.seed(20210316)
 # Specimens ----
 
 spp_didymocarpa <- herbarium_specimens %>%
+  
+  subset_coords(
+    specimen_tbl = .,
+    Longitude = c(-115, 107),
+    Latitude = c(42, 49)
+    ) %>%
+  
   dplyr::filter(
-    grepl(
-      pattern = "Wyoming|Montana|Idaho",
-      x = .data$State
-    ) &
-      (
-        grepl(
-          pattern = "didymocarpa|saximontana|eburniflora",
-          x = .data$prior_id
-        ) &
-          grepl(
-            pattern = "^Physaria$|didymocarpa|saximontana|eburniflora",
-            x = .data$Taxon_a_posteriori
-          )
-      ),
-    .data$Latitude > 44 | .data$Longitude > -110
+    !(Taxon_a_posteriori %in% paste(
+      "Physaria", c("acutifolia", "brassicoides",
+                    "condensata", "dornii", "integrifolia")
+    )) &
+      # Remove specimens with one-off prior identifications.
+      !grepl("geyeri|macrantha|cordiformis|nelsonii", .data$prior_id)
   ) %>%
+  
   dplyr::select(
     "prior_id", "Taxon_a_posteriori",
-    "State", "County", "Latitude", "Longitude", "Ovule_number",
-    "Stem_length_dm", "Basal_leaf_length_cm", 'Mature_fruit_length_mm'
+    "State", "County", "Latitude", "Longitude",
+    "Ovule_number", "Stem_length_dm",
+    "Basal_leaf_length_cm", 'Mature_fruit_length_mm'
+  ) %>%
+  
+  # Add reviewed annotation markdown italicization.
+  dplyr::mutate(
+    Taxon_a_posteriori = .data$Taxon_a_posteriori %>%
+      purrr::map_chr(.x = ., function(taxon) {
+        gsub("Physaria didymocarpa", "P. d.", taxon) %>% paste0("*", ., "*")
+      })
   ) %>%
   
   # Calculate Stem-to-Rosette Ratio
@@ -47,97 +55,7 @@ spp_didymocarpa <- herbarium_specimens %>%
   dplyr::arrange(dplyr::desc(.data$n)) %>%
   dplyr::ungroup()
 
-# Mapping ----
-
-# Function wrapper to plot P. didymocarpa prior and reviewed distributions.
-map_didymocarpa <- function(specimen_tbl, id_column, legend_title, ...) {
-  markdown_labels <-
-    spl_labels(specimen_tbl = specimen_tbl, id_column = id_column)
-  base_map <- ggplot() +
-    layer_borders(
-      spl_extent = spl_bbox(specimen_tbl),
-      sf_county_color = "black"
-    ) +
-    layer_specimens(
-      specimen_tbl = specimen_tbl,
-      id_column = id_column, shape_aes = TRUE
-    ) +
-    scale_color_manual(
-      name = legend_title, labels = markdown_labels,
-      values = ThesisPackage::spp_color, na.value = "black"
-    ) +
-    scale_shape_manual(
-      name = legend_title, labels = markdown_labels,
-      values = ThesisPackage::spp_shape, na.value = 17
-    ) +
-    theme(
-      panel.background = ggplot2::element_blank(),
-      panel.border = ggplot2::element_rect(fill = NA, color =  "black"),
-      legend.key = ggplot2::element_blank(),
-      legend.text = ggtext::element_markdown()
-    )
-  return(base_map)
-}
-
-map_priors <- 
-  map_didymocarpa(
-    specimen_tbl = spp_didymocarpa,
-    id_column = "prior_id",
-    legend_title = "Priors"
-  ) +
-  coord_sf(
-    xlim = range(spl_bbox(spp_didymocarpa)[["Longitude"]]),
-    ylim = range(spl_bbox(spp_didymocarpa)[["Latitude"]])
-  )
-
-map_reviewed <- 
-  map_didymocarpa(
-    specimen_tbl = spp_didymocarpa,
-    id_column = "Taxon_a_posteriori",
-    legend_title = "Annotations"
-  ) +
-  coord_sf(
-    xlim = range(spl_bbox(spp_didymocarpa)[["Longitude"]]),
-    ylim = range(spl_bbox(spp_didymocarpa)[["Latitude"]])
-  )
-
-# Build joint legend with all ID combinations.
-spp_legend <- spp_didymocarpa %>%
-  dplyr::select(
-    "prior_id", "Taxon_a_posteriori",
-    "Latitude", "Longitude"
-  ) %>%
-  tidyr::pivot_longer(
-    cols = c("prior_id", "Taxon_a_posteriori"),
-    values_to = "Taxon"
-    ) %>%
-  map_didymocarpa(
-    specimen_tbl = .,
-    id_column = "Taxon",
-    legend_title = "Annotations"
-  )
-
-FigResultsPdidymocarpaMap <-
-  plot_grid(
-    plot_grid(
-      map_priors + theme(legend.position = "none"),
-      map_reviewed + theme(legend.position = "none"),
-      nrow = 1, labels = c("A", "B")
-    ),
-    get_legend(
-      spp_legend +
-        theme(legend.title.align = 0.5) + 
-        guides(col = guide_legend(ncol = 2))
-    ),
-    nrow = 2, rel_heights = c(3, 1)
-  )
-
-ThesisPackage::save_plot(
-  gg_plot = FigResultsPdidymocarpaMap,
-  height = 4.75, width = 6
-)
-
-# Legend ----
+# Prior ID Legend ----
 
 # Text markdown label replacements to legend.
 trait_labels <- 
@@ -172,14 +90,6 @@ trait_legend <- cowplot::get_legend(trait_identifications)
 
 # Trait ggplots ----
 
-# Add reviewed annotation markdown italicization.
-spp_didymocarpa <- spp_didymocarpa %>%
-  dplyr::mutate(
-    Taxon_a_posteriori = .data$Taxon_a_posteriori %>%
-      purrr::map_chr(.x = ., function(taxon) {
-        gsub("Physaria didymocarpa", "P. d.", taxon) %>% paste0("*", ., "*")
-      })
-  )
 
 # Max Ovules ggplot
 trait_ovules <- ggplot(data = spp_didymocarpa) +
