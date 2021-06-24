220d9bcfa89029bc7f94c58bf69888cfeeefe3b8
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-06-24 16:44:08 -0500

    Resize discussion figure plot margins

--- inst/scripts/DiscussionTraitReplums.R
+++ inst/scripts/DiscussionTraitReplums.R
@@ -3,8 +3,9 @@ library(cowplot)
 library(ggplot2)
 
 # Results: ggplot distribution of specimen Replum Shape.
-trait_replums <-
-  separate_discrete_trait(
+traits <- list()
+traits$counts <-
+  Thesis::separate_discrete_trait(
     specimen_tbl = Thesis::herbarium_specimens,
     trait_selection = "Replum_shape"
   ) %>%
@@ -17,39 +18,73 @@ trait_replums <-
       trait %>%
         gsub("oblanceolate|obovate", "Oblanceolate | Obovate", x = .) %>%
         gsub("elliptic|oblong", "Elliptic | Oblong", x = .) %>%
-        capitalize(character_vector = .)
+        Thesis::capitalize(character_vector = .)
     })
   ) %>% dplyr::distinct()
 
-ggplotTraitReplums <- trait_replums %>%
-  map_trait_distribution(tidy_trait = .) +
-  scale_color_brewer(type = "qual", palette = "Dark2") +
-  labs(color = "Replum Shape")
+traits$ggplot <- traits$counts %>%
+  Thesis::map_trait_distribution(tidy_trait = .) +
+  ggplot2::scale_color_brewer(type = "qual", palette = "Dark2") +
+  ggplot2::labs(color = "Replum Shape")
 
-FigDiscussionTraitReplums <-
-  plot_grid(
-    ggplotTraitReplums +
-      theme(legend.position = "none"),
-    get_legend(ggplotTraitReplums),
-    nrow = 1, rel_widths = c(2, 1)
+traits$map <- traits$ggplot +
+  ggplot2::theme(legend.position = "none")
+
+# .pdf ----
+
+traits$pdf_legend <-
+  cowplot::get_legend(
+    traits$ggplot +
+      ggplot2::guides(color = guide_legend(ncol = 3)) +
+      ggplot2::theme(legend.title.align = 0.5)
   )
+
+traits$figure_pdf <-
+  cowplot::plot_grid(
+    traits$map,
+    traits$pdf_legend,
+    ncol = 1,
+    rel_heights = c(0.9, 0.1)
+  )
+
+# .png ----
+
+traits$png_legend <-
+  cowplot::get_legend(
+    traits$ggplot +
+      ggplot2::guides(color = guide_legend(ncol = 1)) +
+      ggplot2::theme(legend.title.align = 0.5)
+  )
+
+traits$figure_png <-
+  cowplot::plot_grid(
+    traits$map,
+    traits$png_legend,
+    nrow = 1,
+    rel_widths = c(0.8, 0.2)
+  )
+
+# cowplot Grid ----
 
 purrr::pwalk(
   .l = list(
     ext = c("png", "pdf"),
-    width = c(10, 5),
-    height = c(8, 4.5),
-    aspect = c(.167, .167)
+    plot = list(traits$figure_png, traits$figure_pdf),
+    width = c(5, 6),
+    height = c(7, 4),
+    aspect = c(.167, 2.5),
+    row = c(1, 2),
+    col = c(2, 1)
   ),
   .f = function(ext, plot, width, height, aspect, row, col) {
     cowplot::save_plot(
       filename = fs::path("inst/figures/DiscussionTraitReplums", ext = ext),
-      plot = FigDiscussionTraitReplums,
+      plot = plot,
       base_width = width,
       base_height = height,
       base_asp = aspect,
-      nrow = 1,
-      ncol = 1
+      nrow = row,
+      ncol = col
     )
   })
 
