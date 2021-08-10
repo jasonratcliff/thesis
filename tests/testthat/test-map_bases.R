85e3e264861c6a6f48d1ee240903c8c6fa45efa1
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2021-08-09 23:38:12 -0500

    Remove API key from comment
    Fixes #59.
    An updated API key is set from the user `.Renviron` file as
    environment variable "GGMAP_GOOGLE_API_KEY".
    Check for API key with `ggmap::has_google_key()`.
    Lint map_* tests with `styler` using tidyverse style.

--- tests/testthat/test-map_bases.R
+++ tests/testthat/test-map_bases.R
@@ -1,30 +1,49 @@
-test_that("Layer ggmap functions", {
+test_that(desc = "Layer ggmap functions", {
 
   # Test missing API key
-  # Sys.setenv("GGMAP_GOOGLE_API_KEY" = "AIzaSyA2YM4TZn7gEXeby84tJg-CCDYZgkQJd_g")
-  api_key <- Sys.getenv("GGMAP_GOOGLE_API_KEY")
+  api_key <- Sys.getenv(x = "GGMAP_GOOGLE_API_KEY")
   Sys.setenv("GGMAP_GOOGLE_API_KEY" = "")
-  expect_error(layer_ggmap(specimen_tbl = spp_co_front_range,
-                           gg_map_type = "satellite", zoom_lvl = 8))
-  Sys.setenv("GGMAP_GOOGLE_API_KEY" = api_key)  # Reset API key
+  expect_error(
+    layer_ggmap(
+      specimen_tbl = spp_co_front_range,
+      gg_map_type = "satellite", zoom_lvl = 8
+    )
+  )
+  Sys.setenv("GGMAP_GOOGLE_API_KEY" = api_key) # Reset API key
 
   # Test Longitude & Latitude location
-  expect_s3_class(layer_ggmap(specimen_tbl = spp_co_front_range,
-                             gg_map_type = "satellite", zoom_lvl = 8,
-                             gg_longitude = -111, gg_latitude = 42),  "ggplot")
-  expect_error(layer_ggmap(specimen_tbl = spp_co_front_range,
-                           gg_map_type = "satellite", zoom_lvl = 8,
-                           gg_longitude = -111))
+  expect_s3_class(
+    layer_ggmap(
+      specimen_tbl = spp_co_front_range,
+      gg_map_type = "satellite", zoom_lvl = 8,
+      gg_longitude = -111, gg_latitude = 42
+    ),
+    class = "ggplot"
+  )
+  expect_error(
+    layer_ggmap(
+      specimen_tbl = spp_co_front_range,
+      gg_map_type = "satellite", zoom_lvl = 8,
+      gg_longitude = -111
+    )
+  )
 
   # Test Longitude & Latitude midrange
-  expect_s3_class(layer_ggmap(specimen_tbl = Thesis::spp_co_front_range,
-                              gg_map_type = "satellite"), "ggplot")
+  expect_s3_class(
+    layer_ggmap(
+      specimen_tbl = Thesis::spp_co_front_range,
+      gg_map_type = "satellite"
+    ),
+    class = "ggplot"
+  )
 })
 
-test_that("Layer elevation functions", {
+test_that(desc = "Layer elevation functions", {
   elev_ggplot <-
-    layer_elevation(specimen_tbl = Thesis::spp_co_front_range,
-                    raster_zoom = 7, raster_factor = 1)
+    layer_elevation(
+      specimen_tbl = Thesis::spp_co_front_range,
+      raster_zoom = 7, raster_factor = 1
+    )
   expect_s3_class(elev_ggplot, "ggplot")
   expect_s3_class(elev_ggplot$layers[[1]]$geom, "GeomTile")
   expect_s3_class(elev_ggplot$scales$scales[[1]], "ScaleContinuous")
--- tests/testthat/test-map_helpers.R
+++ tests/testthat/test-map_helpers.R
@@ -2,29 +2,43 @@ context("map_helpers.R")
 
 # spl_order() ----
 test_that("Arrange ggplot specimens by count", {
-  specimens_ordered <- spl_order(specimen_tbl = spp_co_front_range,
-                                 id_column = "prior_id")
+  specimens_ordered <- spl_order(
+    specimen_tbl = spp_co_front_range,
+    id_column = "prior_id"
+  )
   top_spp <- spp_co_front_range %>%
-    dplyr::group_by(.data$prior_id) %>% dplyr::tally(x = .) %>%
+    dplyr::group_by(.data$prior_id) %>%
+    dplyr::tally(x = .) %>%
     dplyr::arrange(dplyr::desc(n)) %>%
-    dplyr::slice(1) %>% dplyr::pull("prior_id")
+    dplyr::slice(1) %>%
+    dplyr::pull("prior_id")
   expect_identical(specimens_ordered$prior_id[1], top_spp)
 })
 
 # spl_bbox() ----
 test_that("Bounding box extraction", {
   specimen_bbox <- spl_bbox(herbarium_specimens)
-  expect_equal(specimen_bbox[1, 1],
-               min(herbarium_specimens$Longitude, na.rm = TRUE))
-  expect_equal(specimen_bbox[2, 1],
-               max(herbarium_specimens$Longitude, na.rm = TRUE))
-  expect_equal(specimen_bbox[1, 2],
-               min(herbarium_specimens$Latitude, na.rm = TRUE))
-  expect_equal(specimen_bbox[3, 2],
-               max(herbarium_specimens$Latitude, na.rm = TRUE))
+  expect_equal(
+    specimen_bbox[1, 1],
+    min(herbarium_specimens$Longitude, na.rm = TRUE)
+  )
+  expect_equal(
+    specimen_bbox[2, 1],
+    max(herbarium_specimens$Longitude, na.rm = TRUE)
+  )
+  expect_equal(
+    specimen_bbox[1, 2],
+    min(herbarium_specimens$Latitude, na.rm = TRUE)
+  )
+  expect_equal(
+    specimen_bbox[3, 2],
+    max(herbarium_specimens$Latitude, na.rm = TRUE)
+  )
   specimen_ggmap <-
-    layer_ggmap(specimen_tbl = Thesis::spp_co_front_range,
-                gg_map_type = "satellite")
+    layer_ggmap(
+      specimen_tbl = Thesis::spp_co_front_range,
+      gg_map_type = "satellite"
+    )
   expect_s3_class(spl_bbox(specimen_ggmap), class = "data.frame")
 })
 
@@ -32,8 +46,11 @@ test_that("Bounding box extraction", {
 test_that("Bounding box intersection", {
   states_sfs <- tigris::states() %>% rmapshaper::ms_simplify(input = .)
   states_spp <- herbarium_specimens %>%
-    dplyr::filter(Longitude > -114 & Longitude < -104.5 &
-                    Latitude > 42 & Latitude < 48.75) %>% spl_bbox() %>%
+    dplyr::filter(
+      Longitude > -114 & Longitude < -104.5 &
+        Latitude > 42 & Latitude < 48.75
+    ) %>%
+    spl_bbox() %>%
     spl_states(spl_extent = ., sf_states = states_sfs)
   expect_equal(states_spp, c("Idaho", "Utah", "Wyoming", "Montana"))
 })
@@ -45,8 +62,10 @@ test_that("Specimen identification(s) geoms", {
 
   # Sample specimen collector / collection pairs and aesthetic ID.
   spl_ids <- dplyr::sample_n(
-    tbl = dplyr::select(herbarium_specimens,
-                        Collector, Collection_Number),
+    tbl = dplyr::select(
+      herbarium_specimens,
+      Collector, Collection_Number
+    ),
     size = 50
   )
   id_aes <- sample(c("prior_id", "Taxon_a_posteriori"), 1)
@@ -54,11 +73,12 @@ test_that("Specimen identification(s) geoms", {
   # List of evaluated calls to `spl_id()` with sampled collections.
   spl_calls <-
     purrr::pmap(spl_ids, function(Collector, Collection_Number) {
-      rlang::call2(.fn = rlang::expr(Thesis::spl_id),
-          specimen_tbl = herbarium_specimens,
-          id_column = id_aes, shape_aes = id_aes,
-          collector = Collector, collection = Collection_Number
-        ) %>% rlang::eval_tidy()
+      rlang::call2(
+        .fn = rlang::expr(Thesis::spl_id),
+        specimen_tbl = herbarium_specimens,
+        id_column = id_aes, shape_aes = id_aes,
+        collector = Collector, collection = Collection_Number
+      ) %>% rlang::eval_tidy()
     })
 
   # Walk specimen ID layer calls to check geom slot.
@@ -67,14 +87,11 @@ test_that("Specimen identification(s) geoms", {
       c("GeomPoint", "GeomPoint", "GeomLabel"),
       purrr::map_chr(spl_call, function(spl_geom) {
         class(spl_geom$geom)[[1]]
-        })
-      )
-    })
+      })
+    )
+  })
 
   # Check layering over base ggplot.
   expect_s3_class(object = ggplot2::ggplot() +
-                    spl_calls, class = c("gg", "ggplot"))
-
+    spl_calls, class = c("gg", "ggplot"))
 })
-
-
