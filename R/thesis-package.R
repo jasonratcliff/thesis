5e33a48f292a32f7cf875945463da1d52dff0924
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2023-06-12 22:47:55 -0500

    Remove `mustashe` dependency
    Update `DESCRIPTION` and remove `rprojroot` `@importFrom` statement.

--- R/thesis-package.R
+++ R/thesis-package.R
@@ -6,7 +6,6 @@
 #' @importFrom ggplot2 aes aes_ coord_sf ggplot geom_point theme
 #' @importFrom dplyr filter select add_count distinct
 #' @importFrom purrr keep map walk
-#' @importFrom rprojroot has_file find_root
 #' @importFrom R6 R6Class
 #'
 "_PACKAGE"
