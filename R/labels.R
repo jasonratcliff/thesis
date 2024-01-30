d39fffabc38d0631838d0762739f99aa60be03d8
Jason Ratcliff <jason.ratcliff.515@gmail.com>
2024-01-30 00:35:22 -0600

    New class `Labels` for `tex` voucher labels

---
+++ R/labels.R
@@ -0,0 +1,80 @@
+#' Specimen Voucher Labels
+#'
+#' Format occurrence data for `LaTeX` typesetting of voucher labels.
+#' @export
+Labels <- R6::R6Class(
+  classname = "Labels",
+  private = list(
+    preamble = function(.document = "article") {
+      .document <- match.arg(arg = .document, choices = "article")
+      glue::glue_safe( # LaTeX Preamble
+        "\\documentclass{<<.document>>}",
+        "\\begin{document}",
+        .sep = "\n", .open = "<<", .close = ">>"
+      )
+    },
+    terms = c(
+      "scientificName", "scientificAuthor",
+      "stateProvince", "county", "verbatimLocality",
+      "recordedBy", "recordNumber", "typeStatus",
+      "decimalLatitude", "decimalLongitude",
+      "verbatimElevation"
+    ),
+    label = function(.record) {
+      stopifnot(inherits(.record, "tbl_df") & nrow(.record) == 1)
+      collection <- .record |>
+        dplyr::mutate(
+          state = .record$stateProvince,
+          taxa = .record$scientificName,
+          author = .record$scientificAuthor,
+          type = dplyr::if_else(
+            condition = is.na(.record$typeStatus), true = "",
+            false = glue::glue_data(.record, " \\hfill{{}} {typeStatus}")
+          ),
+          county = .record$county,
+          latitude = .record$decimalLatitude,
+          longitude = .record$decimalLongitude,
+          locale = .record$verbatimLocality,
+          # TODO Handle range of elevelations; unit conversion: m -> f
+          elevation = .record$verbatimElevation,
+          elev_unit = "ft. elev",
+          .keep = "unused"
+        )
+      glue::glue_data_safe(
+        .x = collection,
+        "\\section{Flora of <<state>>}",
+        "\\textit{\\bold{<<taxa>>}} <<author>><<type>>",
+        "<<county>> County: <<locale>>",
+        "<<latitude>>°N <<longitude>>°W <<elevation>> <<elev_unit>>",
+        .open = "<<", .close = ">>", .sep = "\n\n"
+      )
+    }
+  ),
+  public = list(
+    occurrences = NULL,
+    initialize = function(records) {
+      self$occurrences <- records |>
+        dplyr::select(dplyr::all_of(private$terms))
+    },
+    tex = function() {
+      # Map *p-variables* from *n-records* of specimen occurrences.
+      labels <- purrr::pmap(
+        .l = self$occurrences,
+        .f = function(...) {
+          l <- rlang::dots_list(..., .named = TRUE)
+          private$label(.record = tibble::as_tibble(l))
+        }
+      )
+      composed <-
+        list(
+          preamble = private$preamble(),
+          labelled = labels,
+          end = "\\end{document}"
+        )
+      return(composed)
+    },
+    pdf = function(.write = FALSE) {
+      if (.write) stop("Unimplemented")
+    }
+  )
+)
